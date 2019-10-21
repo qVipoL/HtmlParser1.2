44fc1d7b5d3b5db152b905349979b2639c93602d
= <=>
2019-10-21 16:38:08 +0300

    add initial logic

---
+++ Core/HtmlParser/HtmlParser.cs
@@ -0,0 +1,26 @@
+﻿using AngleSharp.Html.Dom;
+using HtmlParser.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlParser.Core.HtmlParser
+{
+    class HtmlParser : IParser<string[]>
+    {
+        public string[] Parse(IHtmlDocument document)
+        {
+            var list = new List<string>();
+            var items = document.QuerySelectorAll("a").Where(item => item.ClassName != null);
+
+            foreach (var item in items)
+            {
+                list.Add(item.TextContent);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ Core/HtmlParser/HtmlParserSettings.cs
@@ -0,0 +1,21 @@
+﻿using HtmlParser.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlParser.Core.HtmlParser
+{
+    class HtmlParserSettings : IParserSettings
+    {
+        public string Url { get; set; }
+        public string Query { get; set; }
+
+        public HtmlParserSettings(string url, string query)
+        {
+            this.Url = url;
+            this.Query = query;
+        }
+    }
+}
