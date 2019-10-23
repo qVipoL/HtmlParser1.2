c95de75b2b245eb8e4b5b0b218afa35e0452286b
= <=>
2019-10-23 17:43:19 +0300

    add html loader

---
+++ Core/HtmlLoader/HtmlLoader.cs
@@ -0,0 +1,36 @@
+﻿using HtmlParser.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlParser.Core.HtmlLoader
+{
+    class HtmlLoader
+    {
+        readonly HttpClient client;
+        readonly string url;
+
+        public HtmlLoader(IParserSettings settings)
+        {
+            this.client = new HttpClient();
+            this.url = settings.Url;
+        }
+
+        public async Task<string> GetSourcePage()
+        {
+            var response = await client.GetAsync(this.url);
+            string source = null;
+
+            if(response != null && response.StatusCode == HttpStatusCode.OK)
+            {
+                source = await response.Content.ReadAsStringAsync();
+            }
+
+            return source;
+        }
+    }
+}
